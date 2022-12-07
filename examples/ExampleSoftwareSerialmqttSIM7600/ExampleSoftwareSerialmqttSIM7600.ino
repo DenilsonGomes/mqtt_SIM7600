0bcc6a0b58dea3b27ccbcb577ded0d86632d2cd2
Denilson Gomes <denilsongomesdg97@gmail.com>
2022-12-07 17:21:44 -0300

    Exemplo atualizado

--- examples/ExampleSoftwareSerialmqttSIM7600/ExampleSoftwareSerialmqttSIM7600.ino
+++ examples/ExampleSoftwareSerialmqttSIM7600/ExampleSoftwareSerialmqttSIM7600.ino
@@ -1,15 +1,34 @@
 #include <mqtt_SIM7600.h>
 
 mqtt_SIM7600 teste(13);
+
+String host = "tcp://demo.thingsboard.io:1883";
+String token = "EKx0Da1Zjiue6N9XkVLf";
+String topico = "v1/devices/me/telemetry";
+String msg = "{\"A\":90}";
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  teste.iniciaMQTT();
-  teste.publica();
+  teste.iniciaMQTT(host, token);
+  delay(3000);
+//  teste.retornoAT();
+  
+  teste.inscreve(topico);
+  delay(3000);
+//  teste.retornoAT();
+  
+  teste.publica(topico, msg);
+  delay(3000);
+//  teste.retornoAT();
+  
   teste.encerraMQTT();
+  delay(3000);
+//  teste.retornoAT();
+  
   delay(60000);
 }
